1c783ec3b92fd7f35146b401c76d01b369f96c5b
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-07-04 21:44:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-// sw.js
+// Basic Service Worker (sw.js)
 const CACHE_NAME = 'ikulu-run-v1';
-const urlsToCache = [
+const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
+  '/manifest.json',
   '/icon-192.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
